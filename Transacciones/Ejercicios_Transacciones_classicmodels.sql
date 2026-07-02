27db51faa8199d67ca242e5aebc57012f08ab26d
Facudevi <alumno27.de.vichenzo.facundo@ipm.edu.ar>
2026-07-02 00:04:45 -0300

    fix: Ejs Transacciones

--- Transacciones/Ejercicios_Transacciones_classicmodels.sql
+++ Transacciones/Ejercicios_Transacciones_classicmodels.sql
@@ -6,41 +6,70 @@ USE classicmodels;
 -- reciba id de cliente, id de producto, cantidad y fecha esperada de envío. Se debe chequear que
 -- haya stock disponible para esa cantidad, de no ser el caso se debe mostrar “Error, stock insuficiente”.
 delimiter //
-CREATE PROCEDURE Compra_Segura(in idcliente int, in idproducto varchar(15),
-								in cantidad int, in fecha_esperada date)
+CREATE PROCEDURE realizar_compra(in id_cliente int, in cod_producto varchar(15), in cantidad int, in fecha_envio date)
 begin
-	declare precio float;
-    declare stockActual int;
+	declare total_stock int;
+    declare precio float;
     declare numOrden int;
     
 	start transaction;
-    SELECT buyPrice, quantityInStock INTO precio, stockActual
+    SELECT buyPrice, quantityInStock INTO precio, total_stock
     FROM products
-    WHERE productCode = idproducto FOR UPDATE;
+    WHERE productCode = cod_producto FOR UPDATE;
     
     SELECT IFNULL(MAX(orderNumber), 0)+1 INTO numOrden
     FROM orders;
     
-    INSERT INTO orders (orderNumber, orderDate, requiredDate, status, customerNumber)
-    VALUE (numOrden, date(now()), fecha_esperada, "In Process", idcliente);
-    INSERT INTO orderdetails VALUES (numOrden, idproducto, cantidad, precio, 1);
+    INSERT INTO orders(orderNumber, orderDate, requiredDate, status, customerNumber)
+    VALUES(numOrden, date(now()), fecha_envio, 'In Process', id_cliente);
+    INSERT INTO orderdetails VALUES(numOrden, cod_producto, cantidad, precio, 1);
     
     UPDATE products SET quantityInStock = quantityInStock - cantidad
-    WHERE productCode = idproducto;
-    IF (stockActual - cantidad) < 0 THEN
+    WHERE productCode = cod_producto;
+    
+    IF (total_stock - cantidad) < 0 THEN
 		rollback;
-		signal sqlstate '45000' SET MESSAGE_TEXT = "ERROR, stock insuficiente";
-    ELSE
-		commit;
-    END IF;
+        signal sqlstate '45000'
+        SET MESSAGE_TEXT = "Error, stock insuficiente";
+	END IF;
+    commit;
 end //
 delimiter ;
 
-call Compra_Segura(278, 'S24_1937', 200, date(now()));
+call realizar_compra(278, 'S24_1937', 200, date(now()));
 
 
 
-#2
+#2.
+-- Crear una transacción que luego de que el cliente realice un pago mayor a $800000, aumente su creditLimit a $1500000.
+-- Asumir que existe una función llamada “simular_pago_tarjeta” que recibe el id de cheque
+-- y devuelve un bool relacionado con el pago aprobado o rechazado del mismo.
+delimiter //
+CREATE PROCEDURE pago_tarjeta(in id_cliente int, in num_check varchar(10), in monto float)
+begin
+	declare pago boolean;
+    declare v_credito float;
+    
+	start transaction;
+    SELECT creditLimit INTO v_credito
+    FROM customers
+    WHERE customerNumber = p_customerNumber FOR UPDATE;
+    
+    INSERT INTO payments VALUES(id_cliente, num_check, date(now()), monto);
+    
+    SET pago = simular_pago_tarjeta(num_check);
+    
+    IF pago = TRUE THEN
+		IF monto > 800000 THEN
+			UPDATE customers SET creditLimit = 1500000
+            WHERE customerNumber = id_cliente;
+		END IF;
+		commit;
+    ELSE 
+		rollback;
+	END IF;    
+end //
+delimiter ;
 
 
 
@@ -49,37 +78,37 @@ call Compra_Segura(278, 'S24_1937', 200, date(now()));
 -- de pedido y cambie el estado del pedido a 'Cancelled'. Debe buscar todos los productos
 -- y cantidades que formaban parte de ese pedido en la tabla orderdetails, y devolver esas
 -- cantidades al stock de cada producto. Se debe chequear que el pedido no esté ya en
--- estado Shipped, si ya fue enviado no se puede cancelar y debe lanzar el mensaje: "Error:
--- No se puede cancelar un pedido que ya fue enviado".
+-- estado Shipped, si ya fue enviado no se puede cancelar y debe lanzar el mensaje: 
+-- "Error: No se puede cancelar un pedido que ya fue enviado"
 delimiter //
-CREATE PROCEDURE Cambiar_Estado_Transaccion(in idOrden int)
+CREATE PROCEDURE cambiar_estado(in num_pedido int)
 begin
-	declare estadoActual text;
-    
-    start transaction;
-    SELECT status INTO estadoActual
+	declare estado varchar(15);
+
+	start transaction;
+    SELECT status INTO estado
     FROM orders
-    WHERE orderNUmber = idOrden FOR UPDATE;
+    WHERE orderNumber = num_pedido FOR UPDATE;
     
-    UPDATE orders SET status = 'cancelled'
-    WHERE orderNumber = idOrden;
+    UPDATE orders SET status = 'Cancelled'
+    WHERE orderNumber = num_pedido;
     
-    UPDATE products p 
+    UPDATE products p
     JOIN orderdetails od ON p.productCode = od.productCode
-    JOIN orders o ON od.orderNumber = o.orderNumber    
+    JOIN orders o ON od.orderNumber = o.orderNumber
     SET p.quantityInStock = p.quantityInStock + od.quantityOrdered
-    WHERE o.orderNumber = idOrden;
+    WHERE orderNumber = num_pedido;
     
-    IF estadoActual = 'Shipped' THEN
+    IF estado = 'Shipped' THEN
 		rollback;
-        signal sqlstate '45000' SET MESSAGE_TEXT = "Error: No se puede cancelar un pedido que ya fue enviado";
-	ELSE
-		commit;
+        signal sqlstate '45000'
+        SET MESSAGE_TEXT = "Error: No se puede cancelar un pedido que ya fue enviado";
 	END IF;
+    commit;
 end //
 delimiter ;
 
-call Cambiar_Estado_Transaccion(10101);
+call cambiar_estado(10101);
 
 
 
@@ -104,10 +133,10 @@ begin
     
     SELECT officeCode INTO oficinaN
     FROM employees
-    WHERE employeeNumber = idNuevo;
+    WHERE employeeNumber = idNuevo FOR UPDATE;
     
     SELECT 1 FROM employees
-    WHERE employeeNUmber = idNuevo;
+    WHERE employeeNumber = idNuevo;
     
     IF (existe = 0) or (oficinaN != oficinaV) THEN
 		rollback;
--- Transacciones/Ejercicios_Transacciones_stock.sql
+++ Transacciones/Ejercicios_Transacciones_stock.sql
@@ -3,24 +3,97 @@ USE stock;
 #1.
 -- Crear una transacción utilizando un procedimiento que reciba el id de un producto, una estantería y una cantidad que se desea retirar.
 -- Se debe modificar la cantidad presente en la estantería y el stock del producto. Se debe validar que las dos cantidades no queden en negativo.
+
+-- No sé qué vendría a ser "estantería"
+    
+    
+    
+#2.
+-- Crear una transacción utilizando un procedimiento que reciba el id de una categoria y
+-- el porcentajeAumento. El procedimiento debe aplicar el aumento a todos los productos
+-- que pertenezcan a esa categoría en la tabla producto. Se debe verificar si la categoría
+-- existe y si tiene productos asociados. Si no existe debe lanzar el error: “Error: categoría inexistente”.
 delimiter //
-CREATE PROCEDURE RegistraIngreso(in ingreso_id int, in proveedor_id int,in codProducto int, in provincia text, in cantidad int)
+CREATE PROCEDURE categoria_aumento(in id_categoria int, in porcentaje_aumento float)
 begin
-	declare provincia_proveedor text;
-	declare existe int;
-    declare numItem int;
+	declare existe, prod_asociados int;
     
     start transaction;
-    SELECT provincia INTO provincia_proveedor
-    FROM proveedor
-    WHERE idProveedor = proveedor_id FOR UPDATE;
+	SELECT 1 INTO existe
+    FROM categoria
+    WHERE idCategoria = id_categoria FOR UPDATE;
     
-    SELECT COUNT(*) INTO existe
-    FROM ingresostock
-    WHERE idIngreso = ingreso_id;
+    SELECT COUNT(*) INTO prod_asociados
+    FROM producto
+    WHERE Categoria_idCategoria = id_categoria FOR UPDATE;
     
     IF existe = 0 THEN
-		INSERT INTO ingresostock(idIngreso, fecha, proveedor_idProveedor)
-        VALUE (NULL, now(), proveedor_id);
+        rollback;
+        signal sqlstate '45000'
+        SET MESSAGE_TEXT = 'Error: categoría inexistente';
+    ELSE IF prod_asociados = 0 THEN
+        rollback;
+        signal sqlstate '45000'
+        SET MESSAGE_TEXT = 'Error: La categoría no tiene productos asociados';
+    ELSE
+        UPDATE producto
+        SET precio = precio * (1 + (p_porcentajeAumento / 100))
+        WHERE id_categoria = p_id_categoria;
+        commit;
+    END IF;
+    END IF;
+end //
+delimiter ;
+
+call categoria_aumento(3, 25);
+
+
+
+#3.
+-- Crear una transacción utilizando un procedimiento que reciba el id de un proveedor, un
+-- código de producto, provincia y la cantidad que llegó en el camión. El procedimiento
+-- debe registrar el ingreso en las tablas ingresostock e ingresostock_producto, actualizar
+-- el stock general del producto y validar que la provincia del proveedor sea la misma que
+-- la que se recibe por parámetro. Si no lo es, debe lanzar el error: "Ingreso rechazado: el
+-- proveedor no está habilitado para operar en esta provincia".
+delimiter //
+CREATE PROCEDURE registrar_ingreso(in id_proveedor int, in cod_prod int, in id_provincia int, in v_cantidad int)
+begin
+	declare v_item int;
+    declare v_idIngreso int;
+    declare v_provincia int;
+    declare v_stock int;
+    
+    start transaction;
+    SELECT idProvincia INTO v_provincia
+    FROM provincia p
+    JOIN proveedor pv ON p.idProvincia = pv.Provincia_idProvincia
+    WHERE pv.idProveedor = id_proveedor FOR UPDATE;
+    
+    SELECT stock INTO v_stock
+    FROM producto
+    WHERE codProducto = cod_prod FOR UPDATE;
+    
+    SELECT IFNULL(MAX(idIngreso), 0)+1 INTO v_idIngreso
+    FROM ingresostock;
+    
+    SELECT IFNULL(MAX(item), 0)+1 INTO v_item
+    FROM ingresostock_producto
+    WHERE Ingreso_idIngreso = v_idIngreso;
+    
+    INSERT INTO ingresostock(idIngreso, fecha, Proveedor_idProveedor)
+    VALUES(v_idIngreso, date(now()), id_proveedor);
+    INSERT INTO ingresostock_producto(item, cantidad, Ingreso_idIngreso, Producto_codProducto)
+    VALUES(v_item, v_cantidad, v_idIngreso, cod_prod);
+    
+    UPDATE producto SET stock = stock + v_cantidad
+    WHERE codProducto = cod_prod;
+    
+    IF v_provincia != id_provincia THEN
+		rollback;
+        signal sqlstate '45000'
+        SET MESSAGE_TEXT = "Ingreso rechazado: el proveedor no está habilitado para operar en esta provincia";
 	END IF;
-    ....
+    commit;
+end //
+delimiter ;
